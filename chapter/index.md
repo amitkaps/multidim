ed759a36cd4d551f37812758089baaeb017ca8c9
Amit Kapoor <amitkaps@gmail.com>
2017-08-23 08:35:42 +0530

    Added initial book

--- chapter/index.md
+++ chapter/index.md
@@ -1,30 +1,26 @@
----
-layout: default
----
-
 # Multi-Dimensional Data Visualisation
 
 ### With Python and R code
 
 ## 1. Introduction
 
-## 2. Handling Data Size (n)
+## 2. Handling Data Size: n
 - For Small Data
 - For Medium Data
 - For Large Data
 - For Big Data
 
-## 3. Handling Dimensions (p)
+## 3. Handling Dimensions: p
 - Coordinate Transforms
 - Dimensionality Reduction
 - Layers and Interaction
 
-## 4. [One Dimension (p = 1)](/chapter04)
+## 4. [One dimension: p = 1](/#04)
 * Categorical: Bar, Column, Stacked, Polar
 * Continuous: Histogram, Frequency Polygon
 * Tips & Tricks: Coordinate changes, Scale transforms, Binwidth
 
-## 5. Two Dimensions (p = 2)
+## 5. Two Dimensions: p = 2
 About: How does one variable compare to another?
 
 * Categorical vs. Categorical: Stacked, Position, Mosaic
@@ -32,21 +28,21 @@ About: How does one variable compare to another?
 * Continuous vs. Continuous: Scatterplot
 * Tips & Tricks: Position, Alpha, Jitter, Binning
 
-## 6. Three to Six Dimensions (p < 7)
+## 6. Three to Six Dimensions: p < 7
 * Multiple Aesthetics - Colour, Shape, Size
 * Facetting - Grid, Wrap
 * Matrix
 
-## 7. Seven to Ten Dimensions (p < 11)
+## 7. Seven to Ten Dimensions: p < 11
 * Parallel Coordinates
 * Projections & Tours
 
-## 8. Dimensionality Reduction (p > 10)
-- Graphical perception and critique
-- Understand color, scales, labeling and annotation
-- Theming and publication ready graphics
+## 8. Dimensionality Reduction: p > 3
+- Basic Concept
+- PCA and LDA
+- t-SNE
 
-## 9. Escaping Flatland (Any p)
+## 9. Escaping Flatland: p > 3
 - Navigation and Dynamic Queries
 - Using linking & brushing, Multiple charts
 - 3D visualization, projections & tours  
---
+++ main.js
@@ -0,0 +1,140 @@
+window.onload = function () {
+
+  const output = document.querySelector("main");  
+  const marked = window.marked;
+  const renderer = new marked.Renderer();
+
+  const defaultFont = "Monaco, Consolas, monospace",
+        defaultSymbolSize = 100,
+        defaultStrokeWidth = 1,
+        defaultColor = '#000',
+        black = "#000",
+        gray = '#888',
+        lightGray = '#ddd';
+
+
+ config = {
+    "autosize": "fit",
+    "background": "whitesmoke",
+    
+    // defaults for basic mark types
+    // each subset accepts mark properties (fill, stroke, etc)
+    "mark": null,
+    "arc": { "fill": defaultColor },
+    "area": { "fill": defaultColor },
+    "image": null,
+    "line": {
+      "stroke": defaultColor,
+      "strokeWidth": defaultStrokeWidth
+    },
+    "path": { stroke: defaultColor },
+    "rect": { fill: defaultColor },
+    "rule": { "stroke": black },
+    "shape": { stroke: defaultColor },
+    "symbol": {
+      "stroke": defaultColor,
+      "size": defaultSymbolSize
+    },
+    "text": {
+      "fill": "black",
+      "font": defaultFont,
+      "fontSize": 11
+    }, 
+
+    "style": {
+      // axis & legend labels
+      "guide-label": {
+        "fill": "black",
+        "font": defaultFont,
+        "fontSize": 10
+      },
+      // axis & legend titles
+      "guide-title": {
+        "fill": "black",
+        "font": defaultFont,
+        "fontSize": 11,
+        "fontWeight": 'bold'
+      },
+      // headers, including chart title
+      "group-title": {
+        "fill": "black",
+        "font": defaultFont,
+        "fontSize": 13,
+        "fontWeight": 'bold'
+      },
+      // defaults for styled point marks in Vega-Lite
+      "point": {
+        "size": defaultSymbolSize,
+        "strokeWidth": defaultStrokeWidth,
+        "stroke": defaultColor,
+        "shape": 'circle'
+      },
+      "circle": {
+        "size": defaultSymbolSize,
+        "fill": defaultColor
+      },
+      "square": {
+        "size": defaultSymbolSize,
+        "strokeWidth": defaultStrokeWidth,
+        "shape": 'square'
+      },
+      "cell": {
+        "stroke": null
+      }
+    },
+    "axis": {
+      "grid": true,
+      "gridColor": 'none',
+      "titlePadding": 10
+    },
+      // defaults for group title
+      "title": {
+        "offset": 5
+    },
+  };
+  
+  const specs = [];
+	opts = {  
+          "mode": "vega-lite",
+          "renderer": "svg",
+          "actions": {export: false, source: false, editor: false},
+          "config": config
+        };
+  
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    headerPrefix: false
+  })
+
+  vegaliteRender = function () {
+    let specs = document.getElementsByClassName('lang-vis')
+		let num = specs.length;
+		for (var i=0; i < num; i++) {
+      el = "#vis-" + i;
+      jsonVis = YAML.parse(specs[i].textContent);
+      console.log(vl.compile(jsonVis).spec == undefined)
+      htmlString = "<div id='vis-" + i + "'></div>"
+      specs[i].parentNode.insertAdjacentHTML('afterend', htmlString);
+      specs[i].style.display = 'none';
+      vega.embed(el, jsonVis, opts);
+    };
+  };
+  
+ 
+  getChapter = function () {
+    let hash = window.location.hash
+    let file = hash.replace("#", "");
+    let fileName = file == "" ? "index.md" : file + ".md";
+    let fileUrl = "chapter/" + fileName;
+
+    fetch(fileUrl).then(data => data.text()).then(data => {
+      output.innerHTML = marked(data)
+      vegaliteRender();
+    });
+  }
+
+
+  window.onhashchange = function () {getChapter();}
+  getChapter();  
+
+}
